4851ad7000de8a35989b9e659df5f612f71b893b
Nguyễn Quang Huy <huynq12@topica.edu.vn>
2019-03-20 11:04:30 +0700

    increase docker version

--- docker-build-update.bat
+++ docker-build-update.bat
@@ -2,7 +2,7 @@
 #docker login docker.topica.vn -u username -p password
 
 #build inheritance
-docker build -f Dockerfile-inheritance -t docker.topica.vn/icovn-prometheus-alert-webhook:1.0.4-SNAPSHOT .
+docker build -f Dockerfile-inheritance -t docker.topica.vn/icovn-prometheus-alert-webhook:1.0.5-SNAPSHOT .
 
 #push
-docker push docker.topica.vn/icovn-prometheus-alert-webhook:1.0.4-SNAPSHOT
+docker push docker.topica.vn/icovn-prometheus-alert-webhook:1.0.5-SNAPSHOT
