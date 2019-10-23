402f404ae5b41f40962e392aa0d1c729c4fce3df
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2019-10-23 14:16:12 +0700

    change config

--- docker-build.bat
+++ docker-build.bat
@@ -1,2 +1,2 @@
-docker build -f Dockerfile -t topkiddocker.topica.vn/icovn-prometheus-alert-webhook:2.0.2-SNAPSHOT .
-docker push topkiddocker.topica.vn/icovn-prometheus-alert-webhook:2.0.2-SNAPSHOT
+docker build -f Dockerfile -t topkiddocker.topica.vn/icovn-prometheus-alert-webhook:2.0.3-SNAPSHOT .
+docker push topkiddocker.topica.vn/icovn-prometheus-alert-webhook:2.0.3-SNAPSHOT
