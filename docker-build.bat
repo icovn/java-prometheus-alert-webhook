8ca6140c08c2c03eb857f18c27b50a6a6fff9367
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2020-04-22 16:36:54 +0700

    change docker repo

--- docker-build.bat
+++ docker-build.bat
@@ -1,2 +1,2 @@
-docker build -f Dockerfile -t topkiddocker.topica.vn/icovn-prometheus-alert-webhook:2.0.4-SNAPSHOT .
-docker push topkiddocker.topica.vn/icovn-prometheus-alert-webhook:2.0.4-SNAPSHOT
+docker build -f Dockerfile -t icovn/icovn-prometheus-alert-webhook:2.0.4-SNAPSHOT .
+docker push icovn/icovn-prometheus-alert-webhook:2.0.4-SNAPSHOT
